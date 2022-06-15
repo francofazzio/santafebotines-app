30ad9ed99b11f4ef2c7bbfb97acbaa3f77f936ff
franco fazzio <francofazzio09@hotmail.com>
2022-06-15 18:57:30 -0300

    subida desafio 4

---
+++ desafio4/rutas/rutas.js
@@ -0,0 +1,68 @@
+const { Router } = require(`express`)
+const router = Router()
+const product = []
+
+class Inventary {
+    constructor (p){
+        this.product = p
+    }
+    async getAll(){
+        return await this.product
+    }
+    async getByid(id){
+        const productFind = this.product.find(i => i.id === id)
+        const error = {error : "producto no encontrado"}
+        if(!productFind){
+            return error
+        }else{
+            return productFind
+        }
+    }
+    static valor = 1
+    
+    async postProduct(obj){
+        let newobj = {...obj , id : Inventary.valor}
+        this.product.push(newobj)
+        Inventary.valor++
+        return newobj.id
+    }
+
+    async putProduct(id){
+        const productFind = this.product.find(i => i.id === id)
+        return productFind
+    }
+
+    async delete(id){
+        const deleteProduct = this.product.filter(i => i.id != id)
+        this.product.push(deleteProduct)
+    }
+}
+
+const base = new Inventary (product)
+
+
+router.get(`/productos`, (req , res)=>{
+    base.getAll().then(r =>res.json(r))
+})
+
+router.get(`/productos/:id`, (req , res)=>{
+    const id = Number(req.params.id)
+    base.getByid(id).then(i => res.status(200).json(i))
+})
+router.post(`/productos`, (req , res )=>{
+    const {title , price , thumbnail  } = req.body
+    base.postProduct({title , price, thumbnail}).then(i => res.send({msg:`el id del producto es  ${i}`}))
+})
+
+router.delete(`/productos/:id` ,(req , res)=>{
+    const id = Number(req.params.id)
+    base.delete(id).then(r => res.send({msg:`producto con id: ${id} borrado `}))
+})
+
+router.put(`/productos/:id` ,(req , res)=>{
+    const id = Number(req.params.id)
+    base.putProduct(id).then(r => res.json({msg: `producto con id: ${id} modificado`}))    
+})
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require(`express`)
+const app = express()
+const rutas = require(`./desafio4/rutas/rutas`)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get(`/app`, (req , res ) => {
+    res.sendFile(__dirname + `/public/index.html`)
+})
+
+app.use(`/app`,rutas)
+
+
+app.listen(8080, ()=> {
+    console.log(`escuchando puerto 8080 `)
+})
